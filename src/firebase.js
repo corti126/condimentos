6b8ec4e05b73a6d032bf2bb1df35b3b87b59ebe5
Santiago <santitati1204@gmail.com>
2026-04-21 02:08:22 -0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaTBm44nTXTmq42mcCLTFl4-U_R5WiA4M",
+  authDomain: "condimentos-14349.firebaseapp.com",
+  projectId: "condimentos-14349",
+  storageBucket: "condimentos-14349.firebasestorage.app",
+  messagingSenderId: "527718016052",
+  appId: "1:527718016052:web:e0d50a62d89466d6345661"
+};
+
+const app = initializeApp(firebaseConfig);
